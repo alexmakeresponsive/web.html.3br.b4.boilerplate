e1ba41bd688322a15223a4054e4a5ca31d10c2fb
Gorchakov Alexandr <gradual.development.mail@gmail.com>
2017-08-22 12:48:31 +0600

    html: page-end.php: include jQuery

--- files-without-cms/source/php/parts/page-end.php
+++ files-without-cms/source/php/parts/page-end.php
@@ -1,5 +1,6 @@
-
+        <script src="../../node_modules/jquery/dist/jquery.js"></script>
         <script src="../../node_modules/bootstrap/dist/js/bootstrap.js" ></script>
+
         <script src="../resources/template/scripts/assembled/scripts.js"></script>
     </body>
 </html>
