c39453f615085786983b492c2e32ec965ea3b102
Gorchakov Alexandr <gradual.development.mail@gmail.com>
2017-08-22 12:49:46 +0600

    html: update header-1.php, footer-1.php

---
+++ files-without-cms/source/php/parts/headers/header-1.php
@@ -0,0 +1,43 @@
+<header class="header-1">
+    <div class="container-fluid">
+        <div class="row">
+
+            <div class="col">
+                <img src="../assets/template/images/header/OM6QGG0.png" width="100" alt="header-logo">
+            </div>
+
+            <div class="col-6">
+                <h2>Header title</h2>
+                <p>Header description</p>
+            </div>
+
+            <div class="col">
+                <div class="d-flex flex-row-reverse">
+                    <div>
+                        <div class="d-flex justify-content-end">
+                            <button class="buttons  button-nav-menu-1" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                                <svg width="32" height="32" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1664 1344v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45z"/></svg>
+                            </button>
+                        </div>
+                        <div>
+                            <div class="collapse" id="collapseExample">
+                                <ul class="text-right">
+                                    <li>
+                                        <a href="#">Long name for Home page</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">About</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">404</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</header>
