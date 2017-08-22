b119bac5d2b77d34941a1d184d7f04b6b12d9312
Gorchakov Alexandr <gradual.development.mail@gmail.com>
2017-08-22 12:49:59 +0600

    html: update home.php

---
+++ files-without-cms/source/php/pages/home.php
@@ -0,0 +1,22 @@
+<div class="page-wrapper  d-flex flex-column  justify-content-between">
+
+    <div>
+        <?php require_once( 'parts/headers/header-1.php' ); ?>
+
+        <div class="pages  home">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col">
+                        <h1>This is home page</h1>
+                        <p>
+                            You use gradual development starter kit with Bootstrap framework.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <?php require_once( 'parts/footers/footer-1.php' ); ?>
+</div>
