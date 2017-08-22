06a9b1af5bea15c39e1612aee116edf33473630e
Gorchakov Alexandr <gradual.development.mail@gmail.com>
2017-08-22 11:16:59 +0600

    html: update page-head and page-end

--- files-without-cms/source/php/parts/page-end.php
+++ files-without-cms/source/php/parts/page-end.php
@@ -1,3 +1,5 @@
 
+        <script src="../../node_modules/bootstrap/dist/js/bootstrap.js" ></script>
+        <script src="../resources/template/scripts/assembled/scripts.js"></script>
     </body>
 </html>
--- files-without-cms/source/php/parts/page-head.php
+++ files-without-cms/source/php/parts/page-head.php
@@ -3,6 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title></title>    
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+    <title></title>
+
+    <link rel="stylesheet" href="../../node_modules/bootstrap/dist/css/bootstrap-reboot.css">
+    <link rel="stylesheet" href="../../node_modules/bootstrap/dist/css/bootstrap.css">
+
+    <link rel="stylesheet" href="../resources/template/styles/css/styles.css">
 </head>
 <body>
+
+    <!--[ if lt IE10 ]>
+        <p>Пожалуйста обновите свой браузер</p>
+    <![ endif ]-->
